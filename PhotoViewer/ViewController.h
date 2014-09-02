f76ef8c684dfe7b574c11fc0a975daa8cd15cec8
Max Campolo <maxcampolo@maxs-mbp.home>
2014-09-02 18:44:45 -0400

    Initial Commit

---
+++ PhotoViewer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhotoViewer
+//
+//  Created by Max Campolo on 9/2/14.
+//  Copyright (c) 2014 Maxim Campolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PhotoViewer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhotoViewer
+//
+//  Created by Max Campolo on 9/2/14.
+//  Copyright (c) 2014 Maxim Campolo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
